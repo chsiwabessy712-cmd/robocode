7c8f1e202a0dbc3fd7fb4737f0d4ee386dccff60
chsiwabessy712-cmd <chsiwabessy712@gmail.com>
2026-05-08 05:15:55 +0700

    Update settings.py

--- website1/settings.py
+++ website1/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--_4gfc!ba&rcsihs2^f5u9y5(@fu3ov9azwj-_x!a+fotk-78c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG','FALSE').lower() in ['true','1','yes']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
